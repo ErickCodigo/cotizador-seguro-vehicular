52bf6fed398554790857796c5d811c116f8c2957
Erick Hilario León <erprogramador@gmail.com>
2021-05-04 06:13:49 -0500

    create invariant properties

---
+++ src/specs/valuesBreakpoints.ts
@@ -0,0 +1,18 @@
+import {ValuesBreakpoints as ValuesBreakpointsInterface} from "./models/ValuesBreakpoints";
+
+export const valuesBreakpoints: ValuesBreakpointsInterface = {
+    sm: '640px',
+    // => @media (min-width: 640px) { ... }
+
+    md: '768px',
+    // => @media (min-width: 768px) { ... }
+
+    lg: '1024px',
+    // => @media (min-width: 1024px) { ... }
+
+    xl: '1280px',
+    // => @media (min-width: 1280px) { ... }
+
+    '2xl': '1536px',
+    // => @media (min-width: 1536px) { ... }
+};
