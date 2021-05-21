600c5153afddba480e2e607de8d5bb29d6b40987
Erick <erprogramador@gmail.com>
2021-05-21 05:27:11 -0500

    config env for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-    mode: 'jit',
     // Optimizar: Elimina las clases sin usar
     purge: [
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}'
     ],
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+    },
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
